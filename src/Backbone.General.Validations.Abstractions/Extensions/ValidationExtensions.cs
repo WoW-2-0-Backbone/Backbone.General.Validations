bd3a1038dd0f0edf6c89fd4fa37d28d1ddd31a5f
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-09-10 21:40:26 +0500

    Added validation extensions

---
+++ src/Backbone.General.Validations.Abstractions/Extensions/ValidationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Backbone.General.Validations.Abstractions.Exceptions;
+
+namespace Backbone.General.Validations.Abstractions.Extensions;
+
+/// <summary>
+/// Contains extensions for validations.
+/// </summary>
+public static class ValidationExtensions
+{
+    /// <summary>
+    /// Attempts to check and get validation exception from app exception.
+    /// </summary>
+    /// <param name="exception">The base exception to check if it contains any validation exceptions.</param>
+    /// <param name="validationException">Found validation exception.</param>
+    /// <returns>True, if any validation exception is found, otherwise false.</returns>
+    public static bool TryGetValidationException(this Exception exception, out AppValidationException? validationException)
+    {
+        var baseException = exception;
+        while (baseException != null)
+        {
+            if (baseException is AppValidationException appValidationException)
+            {
+                validationException = appValidationException;
+                return true;
+            }
+
+            baseException = baseException.InnerException;
+        }
+
+        validationException = null;
+        return false;
+    }
+}
