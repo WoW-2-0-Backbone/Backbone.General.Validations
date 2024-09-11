a7e12d5f8b86678c32fe049feb075e2a6f8c579a
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-09-11 20:31:04 +0500

    Added validations abstractions package

--- src/Backbone.General.Validations.Abstractions/Settings/ValidationSettings.cs
+++ src/Backbone.General.Validations.Abstractions/Settings/ValidationSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace Backbone.General.Validations.Abstractions.Settings;
 
 /// <summary>
-/// Represents the settings for validation.
+/// Represents the basic settings for validation.
 /// </summary>
 public sealed record ValidationSettings;
