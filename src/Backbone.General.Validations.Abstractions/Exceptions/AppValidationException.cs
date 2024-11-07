c74b236719f4894fd8794a9ef71014a6379a7fa6
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-11-07 20:37:14 +0500

    Separated app validation exception interface

--- src/Backbone.General.Validations.Abstractions/Exceptions/AppValidationException.cs
+++ src/Backbone.General.Validations.Abstractions/Exceptions/AppValidationException.cs
@@ -1,4 +1,5 @@
-
+using System.ComponentModel.DataAnnotations;
+using System.Net;
 using Backbone.General.Exceptions.Abstractions.Exceptions.General;
 
 namespace Backbone.General.Validations.Abstractions.Exceptions;
@@ -6,22 +7,44 @@ namespace Backbone.General.Validations.Abstractions.Exceptions;
 /// <summary>
 /// Represents application validation exception.
 /// </summary>
-public class AppValidationException : AppException
+public class AppValidationException : AppException, IAppValidationException
 {
-    /// <inheritdoc />
-    public AppValidationException(Exception exception, bool isPublicValidation = true) : base("Validation failed", exception)
+  /// <summary>
+    /// Initializes a new instance of the <see cref="AppValidationException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="statusCode">Optional HTTP status code for this exception.</param>
+    /// <param name="meta">Optional metadata for additional information.</param>
+    public AppValidationException(
+        string message,
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest,
+        Dictionary<string, string>? meta = null)
+        : base(message, statusCode, meta)
     {
-        IsPublicValidation = isPublicValidation;
-    }
-
-    /// <inheritdoc />
-    public AppValidationException(string message, Exception exception, bool isPublicValidation = true) : base(message, exception)
-    {
-        IsPublicValidation = isPublicValidation;
+        ValidationResults = Array.Empty<ValidationResult>();
     }
 
     /// <summary>
-    /// Determines if the validation is public and should be shown to the user.
+    /// Initializes a new instance of the <see cref="AppValidationException"/> class with a specified error message, validation results, and optional metadata.
     /// </summary>
-    public bool IsPublicValidation { get; set; }
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="validationResults">A collection of validation results, either from DataAnnotations or FluentValidation.</param>
+    /// <param name="statusCode">Optional HTTP status code for this exception (default is 400 Bad Request).</param>
+    /// <param name="meta">Optional metadata for additional information.</param>
+    public AppValidationException(
+        string message,
+        IEnumerable<ValidationResult> validationResults,
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest,
+        Dictionary<string, string>? meta = null)
+        : base(message)
+    {
+        StatusCode = statusCode;
+        Meta = meta ?? new Dictionary<string, string>();
+        ValidationResults = validationResults;
+    }
+    
+    /// <summary>
+    /// Gets the validation results that caused the exception.
+    /// </summary>
+    public IEnumerable<ValidationResult> ValidationResults { get; init; }
 }
